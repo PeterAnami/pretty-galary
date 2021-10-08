57dd315f50549eee7ad229c239c43e7b50055604
peterAnami <peter.andake@student.moringaschool.com>
2021-10-08 13:03:48 +0300

    file definition

--- mypictures/views.py
+++ mypictures/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http.response import Http404, HttpResponse
+import datetime as dt
 
 # Create your views here.
+def home(request):
+  return render(request, 'index.html')
