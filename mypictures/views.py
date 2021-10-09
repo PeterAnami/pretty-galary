754a11bcda7fc7cdd6c623939f7307d76c5d4329
peterAnami <peter.andake@student.moringaschool.com>
2021-10-09 21:39:43 +0300

    modifivation to galary,venv,pics

--- mypictures/views.py
+++ mypictures/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http.response import Http404, HttpResponse
-import datetime as dt
 
 # Create your views here.
 def home(request):
-  return render(request, 'index.html')
+    return render(request, 'index.html')
