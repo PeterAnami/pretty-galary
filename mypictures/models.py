3deab9bd5a2e27916474493958b12e655e346c1a
peterAnami <peter.andake@student.moringaschool.com>
2021-10-11 12:52:09 +0300

    modified models file

--- mypictures/models.py
+++ mypictures/models.py
@@ -18,3 +18,89 @@ class meta:
 
     def save_owner(self):
      self.save()
+
+class Location(models.Model):
+  title = models.CharField(max_length =50)
+
+  def save_location(self):
+    self.save()
+
+  def delete_location(self):
+      self.delete()
+
+  def update_location(self, update):
+    self.title = update
+    self.save()
+
+  @classmethod
+  def get_location_id(cls, id):
+    location_id = Location.objects.get(pk = id)
+    return location_id
+
+  def __str__(self):
+    return self.title
+
+class Category(models.Model):
+  title = models.CharField(max_length =50)
+
+  def save_category(self):
+    self.save()
+
+  def delete_category(self):
+    self.delete()
+
+  def update_category(self, update):
+    self.title = update
+    self.save()
+
+  @classmethod
+  def get_category_id(cls, id):
+    category_id = Category.objects.get(pk = id)
+    return category_id
+
+  def __str__(self):
+    return self.title
+
+class Photo(models.Model):
+  title = models.CharField(max_length=60)
+  description = models.TextField()
+  owner = models.ForeignKey(Owner,on_delete=models.CASCADE)
+  posted_at = models.DateTimeField(auto_now_add=True)
+  image = CloudinaryField('image')
+  location = models.ForeignKey(Location,on_delete=models.CASCADE)
+  category = models.ForeignKey(Category,on_delete=models.CASCADE)
+
+  def save_photo(self):
+    self.save()
+
+  def delete_photo(self):
+    self.delete()
+
+  @classmethod
+  def update_photo(cls, id ,title,description ,owner, location, category):
+    update = cls.objects.filter(id = id).update(title = title,description = description,owner=owner,location = location,category = category)
+    return update
+
+  @classmethod
+  def get_all_photos(cls):
+    photos = cls.objects.all()
+    return photos
+
+  @classmethod
+  def get_photo_id(cls,id):
+    photo_id = cls.objects.filter(id= id).all()
+    return photo_id
+
+  @classmethod
+  def filter_photo_by_location(cls,location):
+    photos = Photo.objects.filter(location__title__icontains=location)
+    return location
+
+  @classmethod
+  def search_photos_by_category(cls,category):
+      photos = Photo.objects.filter(category__title__icontains=category)
+      return photos
+
+
+  def __str__(self):
+    return self.title
