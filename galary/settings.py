8a5d17e813dc62ac7431e5e39a1cda365b5200e4
peterAnami <peter.andake@student.moringaschool.com>
2021-10-11 10:41:55 +0300

    migration

--- galary/settings.py
+++ galary/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mypictures',
-    # 'bootstrap3',
+    'bootstrap3',
     
 ]
 
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'galary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pictures',
+        'USER':'moringa',
+        'PASSWORD':'Access',
     }
 }
 
@@ -108,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
